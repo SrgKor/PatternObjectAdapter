1ccc2d79cd27958863c21ab058412a2e094bfcdc
Sergey Korolev <humankind@yandex.ru>
2014-11-18 15:46:21 +0300

    Шаблон паттерна Object Adapter

---
+++ Pattern ObjectAdapter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pattern_ObjectAdapter
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            ITargetInterface s;
+            Adaptee adaptee = new Adaptee();
+            ObjectAdapter adapter = new ObjectAdapter(adaptee);
+            s = adapter;
+            s.targetMethod();
+
+        }
+    }
+
+        interface ITargetInterface
+        {
+            void targetMethod();
+        }
+
+        class ObjectAdapter : ITargetInterface
+        {
+            public ObjectAdapter(Adaptee adaptee)
+            {
+                this.adaptee = adaptee;
+            }
+
+            private Adaptee adaptee;
+
+            public void targetMethod() //вызов метода через целевой интерфейсный метод
+            {
+                adaptee.method();
+            }
+        }
+
+        class Adaptee   
+        {
+            public void method()
+            {
+                Console.WriteLine("Наш метод");
+                Console.ReadLine();
+            }
+        }
+
+        
+    
+}
